3768d64e3fe62762f03de72ee0c9305f63fd5773
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-04-24 07:49:08 +0200

    Add tests to the geometry module

--- geometry.erl
+++ geometry.erl
@@ -1,8 +1,13 @@
 -module(geometry).
 
--export([area/1]).
+-export([area/1, test/0]).
 
 area({rectangle, Width, Height}) ->
   Width * Height;
 area({square, Side}) ->
   Side * Side.
+
+test() ->
+  12 = area({rectangle, 3, 4}),
+  144 = area({square, 12}),
+  tests_worked.
